772a65740678465b85bfc68f165f71149969bc59
Daniel Lehmann <daniel.lehmann188@gmail.com>
2025-04-01 20:02:03 +0200

    login-page done, login button on top right needs removing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 
 export const routes: Routes = [
     { path: '', component: LandingPageComponent },
+    { path: 'login', component: LoginPageComponent },
 ];
