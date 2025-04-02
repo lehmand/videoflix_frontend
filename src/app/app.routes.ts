439d2269de6db60944ab340513bf3f509556b489
Daniel Lehmann <daniel.lehmann188@gmail.com>
2025-04-02 20:20:41 +0200

    added all pre pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { LandingPageComponent } from './landing-page/landing-page.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { SignupPageComponent } from './signup-page/signup-page.component';
 import { ForgotPageComponent } from './forgot-page/forgot-page.component';
+import { ResetPageComponent } from './reset-page/reset-page.component';
 
 export const routes: Routes = [
     { path: '', component: LandingPageComponent },
     { path: 'login', component: LoginPageComponent },
     { path: 'signup', component: SignupPageComponent},
     { path: 'forgot-password', component: ForgotPageComponent},
+    { path: 'reset-password', component: ResetPageComponent}, 
 ];
---
+++ src/app/reset-page/reset-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reset-page',
+  imports: [],
+  templateUrl: './reset-page.component.html',
+  styleUrl: './reset-page.component.scss'
+})
+export class ResetPageComponent {
+
+}
