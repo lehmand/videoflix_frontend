2d417670c1edde73cb7aa65379be3390f0fe625b
Daniel Lehmann <daniel.lehmann188@gmail.com>
2025-04-15 00:06:25 +0200

    sending emails, registration works, added toast message and toast-service

--- src/app/services/auth-service/auth.service.ts
+++ src/app/services/auth-service/auth.service.ts
@@ -7,22 +7,29 @@ import { Observable } from 'rxjs';
 })
 export class AuthService {
   
-  BASE_URL = 'http://127.0.0.1:8000/';
-  AUTH_URL = 'auth/'
+  BASE_URL = 'http://127.0.0.1:8000/auth/';
   sessionEmail: string | null;
 
   constructor(
-    private httpService: HttpService
+    private httpService: HttpService 
   ) {
       this.sessionEmail = ''
    }
 
 
   checkUser(email: string): Observable<any> {
-    return this.httpService.post(`${this.BASE_URL}${this.AUTH_URL}check/`, { email })
+    return this.httpService.post(`${this.BASE_URL}check/`, { email })
   }
 
   getEmail(){
     this.sessionEmail = sessionStorage.getItem('email')
+  }
+
+  registration(data: any){
+    return this.httpService.post(`${this.BASE_URL}registration/`, data)
+  }
+
+  login(data: any){
+    return this.httpService.post(`${this.BASE_URL}login/`, data)
   }
 }
--- src/app/signup-page/signup-page.component.ts
+++ src/app/signup-page/signup-page.component.ts
@@ -1,11 +1,38 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../services/auth-service/auth.service';
+import { CommonModule } from '@angular/common';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { NavigationService } from '../services/navigation-service/navigation.service';
 
 @Component({
   selector: 'app-signup-page',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './signup-page.component.html',
   styleUrl: './signup-page.component.scss'
 })
 export class SignupPageComponent {
 
+  constructor(
+    private authService: AuthService,
+    private navigator: NavigationService
+  ){}
+
+  signUpForm = new FormGroup({
+    email: new FormControl('',[ Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    repeated_password: new FormControl('', [Validators.required])
+  })
+
+  register() {
+    this.authService.registration(this.signUpForm.value).subscribe({
+      next: (response) => {
+        console.log('Registration success', response)
+        this.navigator.navigateTo('login')
+      },
+      error: (error) => {
+        console.error('Registration failed', error)
+        return
+      }
+    })
+  }
 }
