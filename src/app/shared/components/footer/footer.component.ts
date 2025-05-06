d51a2e43e1bf2d18bf89ee718dbbe272552036b4
Daniel Lehmann <daniel.lehmann188@gmail.com>
2025-05-06 21:55:21 +0200

    added imprint

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,6 +1,6 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { NavigationService } from '../../../services/navigation-service/navigation.service';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { VideoService } from '../../../services/video-service/video.service';
 import { Router, RouterLink } from '@angular/router';
 
@@ -10,10 +10,27 @@ import { Router, RouterLink } from '@angular/router';
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
+
+  isSpecialPage: boolean = false;
 
   constructor(
     public navigation: NavigationService,
     public videoService: VideoService,
-  ){}
+    public router: Router
+  ){
+    this.checkCurrentRoute()
+  }
+
+  ngOnInit(): void {
+    this.checkCurrentRoute()
+    this.router.events.subscribe(() => {
+      this.checkCurrentRoute()
+    })
+  }
+
+  checkCurrentRoute() {
+    const currentUrl = this.router.url;
+    this.isSpecialPage = currentUrl.includes('/privacy') || currentUrl.includes('/imprint')
+  }
 }
--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 import { AuthService } from '../../../services/auth-service/auth.service';
 import { CommonModule } from '@angular/common';
@@ -11,16 +11,26 @@ import { VideoService } from '../../../services/video-service/video.service';
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
 
   logoSrc: string = 'assets/img/logo.svg'
+  isSpecialPage: boolean = false;
 
   constructor(
     public authService: AuthService,
     private router: Router,
     public videoService: VideoService,
+    public navigation: NavigationService,
   ){
-    this.updateLogo()
+    this.updateLogo();
+    this.checkCurrentRoute();
+  }
+
+  ngOnInit(): void {
+    this.checkCurrentRoute()
+    this.router.events.subscribe(() => {
+      this.checkCurrentRoute()
+    })
   }
 
   @HostListener('window:resize', [])
@@ -34,6 +44,16 @@ export class HeaderComponent {
 
   logout(){
     this.authService.logout()
+  }
+
+  goBack(){
+    this.navigation.goBack()
+    this.isSpecialPage = false;
+  }
+
+  checkCurrentRoute() {
+    const currentUrl = this.router.url;
+    this.isSpecialPage = currentUrl.includes('/privacy') || currentUrl.includes('/imprint')
   }
 
   checkUserLogin(){
